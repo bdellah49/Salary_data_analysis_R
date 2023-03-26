71071a676d2e1add81311e30d60881f9225fe729
abdellah beliacine <38139495+bdellah49@users.noreply.github.com>
2023-03-26 12:02:28 +0100

    Add files via upload

---
+++ Exo1_Rstudio.R
@@ -0,0 +1,26 @@
+dataset=read.csv("C:/Users/hp/Downloads/Data/Data/Salary_Data.csv")
+print(dataset)
+#install.packages("caTools")
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Salary,SplitRatio=2/3)
+training_set=subset(dataset,split==TRUE)
+test_set=subset(dataset,split=FALSE)
+regressor=lm(formula=Salary ~ YearsExperience,data=training_set)
+y_pred=predict(regressor,newdata=test_set)
+#install.packages("ggplot2")
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=training_set$YearsExperience,y=training_set$Salary),color='red')+
+  geom_line(aes(x=training_set$YearsExperience,y=predict(regressor,newdata=training_set)),color='blue')+
+  ggtitle('salary vs Experience (Training set)')+
+  xlab('Years of Experience')+
+  ylab('Salary')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=test_set$YearsExperience,y=test_set$Salary),color='red')+
+  geom_line(aes(x=training_set$YearsExperience,y=predict(regressor,newdata=training_set)),color='blue')+
+  ggtitle('salary vs Experience (Test set)')+
+  xlab('Years of Experience')+
+  ylab('Salary')
+
